c3294882f18a42259a2c4edb8bccd56c7708ab33
DavidG444 <114938711+DavidG444@users.noreply.github.com>
2022-12-02 21:14:55 -0500

    Create Main.java

---
+++ 06-shapes/Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(final String[] args) {
+    sentence(6, 4);
+  }
+
+  public static void sentence(final int l, final int w) {
+    for (int row = 0; row < w; row++) {
+      for (int col = 0; col < l; col++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+
+class Main{
+  public static void main(String [] args){
+    rightTriangle(4);
+  }
+  public static void rightTriangle(int s){
+    int p = 0;
+    for(int row = 0; )
+    for(int col=0; col <=p; col++){
+      
+    }
+  }
+}
