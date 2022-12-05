8ad12034192bf18fe318da203d24db017e9b6671
DavidG444 <114938711+DavidG444@users.noreply.github.com>
2022-12-05 12:04:46 -0500

    Create Main.java

---
+++ 05-div-by-3/Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+   divby3(1234);//True
+
+  }//end of main method
+
+  public static void divby3(int div){
+    int sum = 0;
+
+    while (div != 0) {
+      sum += div % 10;
+      div = div / 10;
+    }
+  
+    if(sum % 3 == 0) {
+      System.out.println("This number divisible by 3");
+    }
+    else {
+      System.outprintln("This number is not divisible by 3");
+    }
+  }
+}
